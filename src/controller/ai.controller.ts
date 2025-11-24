a6d5fc2392bd7475aa487e222230002ddf497c9e
Alokafernando <buddhikaaloka2005@gmail.com>
2025-11-24 11:48:32 +0530

    update

--- src/controller/ai.controller.ts
+++ src/controller/ai.controller.ts
@@ -1,42 +1,47 @@
 import axios from "axios"
 import { Request, Response } from "express"
+import dotenv from "dotenv"
+dotenv.config()
 
-export const genrateContent = async (req: Request, res: Response) => {
-  try {
-    const { text, maxToken } = req.body
+const AI_KEY = process.env.AI_API_KE as String
 
-    // check text null send 400
-    const aiResponse = await axios.post(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent",
-      {
-        contents: [
-          {
-            parts: [{ text }]
-          }
-        ],
-        generationConfig: {
-          maxOutputTokens: maxToken || 150
+export const generateContent = async (req: Request, res: Response) => {
+    try {
+        const { text, maxToken } = req.body
+
+        if (!text) {
+            // return res.status(403).json({message: "text is null"})
         }
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "X-goog-api-key": ""
-        }
-      }
-    )
 
-    const genratedContent =
-      aiResponse.data?.candidates?.[0]?.content?.[0]?.text ||
-      aiResponse.data?.candidates?.[0]?.content?.parts?.[0]?.text ||
-      "No data"
+        const aiResponse = await axios.post("https://generativelanguage.googleapis.com/v1beta/gemini-2.0-flash:streamGenerateContent",
+            {
+                content: [
+                    {
+                        parts: [{ text }]
+                    }
+                ], generationConfig: {
+                    maxOutputTokens: maxToken || 150
+                }
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    "x-goop-api-key": "AI_KEY"
+                }
+            }
+        )
 
-    console.log(res)
+        const genratedContent =
+            aiResponse.data?.candidates?.[0]?.content?.[0]?.text ||
+            aiResponse.data?.candidates?.[0]?.content?.parts?.[0]?.text ||
+            "No data"
 
-    res.status(200).json({
-      data: genratedContent
-    })
-  } catch (err) {
-    res.status(500).json({ message: "AI genration failed" })
-  }
+        console.log(res)
+
+        res.status(200).json({
+            data: genratedContent
+        })
+    } catch (error) {
+        res.status(500).json({ message: "Ai generation failed" })
+    }
 }
